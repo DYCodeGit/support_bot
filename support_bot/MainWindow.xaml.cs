26d3e7a8adec708af7cb3a7af0defa1ed1587797
DYCodeGit <dimaonetoo@gmail.com>
2021-05-18 20:48:09 +0300

    Добавьте файлы проекта.

---
+++ support_bot/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telegram.Bot;
+using System.IO;
+
+namespace support_bot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TelegramBotClient bot;
+        ObservableCollection<TelegUser> Users;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Users = new ObservableCollection<TelegUser>(); //create collection
+            usersList.ItemsSource = Users;
+
+            string token = "1777247596:AAGRCYcrb5XLj0AMfkEOKJkb_LeBqtuhx-8";
+            bot = new TelegramBotClient(token);
+
+            bot.OnMessage += delegate (object sender, Telegram.Bot.Args.MessageEventArgs e)
+            {
+                
+                
+
+                this.Dispatcher.Invoke(() =>
+                    {
+                        // add in collection
+                        var person = new TelegUser(e.Message.Chat.FirstName, e.Message.Chat.Id);
+                        if (!Users.Contains(person)) Users.Add(person);
+                        Users[Users.IndexOf(person)].AddMesseges($"{person.Nick}: {e.Message.Text}");
+
+                    });
+
+            };
+
+            bot.StartReceiving();
+
+            buttonSend.Click += delegate { Send(); };
+            textBoxMsg.KeyDown += (s, e) => {
+                if (e.Key == Key.Return)
+                {
+                    Send();
+                }
+            };
+
+        }
+
+        public void Send()
+        {
+            var concreteUser = Users[Users.IndexOf(usersList.SelectedItem as TelegUser)];
+            string responseMsg = $"Support: {textBoxMsg.Text}";
+            concreteUser.Messages.Add(responseMsg);
+
+            bot.SendTextMessageAsync(concreteUser.Id, textBoxMsg.Text);
+
+            textBoxMsg.Text = String.Empty;
+        }
+
+        
+    }
+}
---
+++ support_bot/TelegUser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using System.IO;
+using System.Windows.Shapes;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Collections;
+
+namespace support_bot
+{
+    public class TelegUser : INotifyPropertyChanged, IEquatable<TelegUser>
+    {
+        public TelegUser(string Nickname, long CharId)
+        {
+            this.nick = Nickname;
+            this.id = CharId;
+            Messages = new ObservableCollection<string>();
+             
+        }
+
+        private string nick;
+        public string Nick
+        {
+            get { return this.nick; }
+            set { this.nick = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Nick))); }
+        }
+
+        private long id;
+        public long Id
+        {
+            get { return this.id; }
+            set { this.id = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Id))); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged; // messeges elem window
+
+
+        public bool Equals(TelegUser other) => other.Id == this.id; // user vs user
+
+        public ObservableCollection<string> Messages { get; set; } // all messeges
+
+        public void AddMesseges(string Text) => Messages.Add(Text); // add messeges
+
+
+       
+
+        
+    }
+}
